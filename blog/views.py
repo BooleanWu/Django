66265ee66f68338d0f0df84bbabacc23aea5d64d
Boolean <nizhaosimazzz@163.com>
2018-09-09 12:52:38 +0800

    add markdow edit

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .form import PostForm
+from markdown import markdown
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')  
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk = pk)
+    post.text = markdown(post.text,extensions=[
+                                     'markdown.extensions.extra',
+                                     'markdown.extensions.codehilite',
+                                     'markdown.extensions.toc',
+                                  ])
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):   
--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'mdeditor',
-    'markdown_deux',
 ]
 
 MIDDLEWARE = [
